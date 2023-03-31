c4a4cac83b44acf6a8d2c472b96b9560605b07a0
junwensoh <junwen.soh@thecarousell.com>
2023-03-31 10:35:17 +0800

    Custom hooks practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Message from './Message';
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./Message";
+import { useFetch } from "usehooks-ts";
+const url = `http://jsonplaceholder.typicode.com/posts`;
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 function App() {
+  const { data, error } = useFetch<Post[]>(url);
+  if (error) return <p>There is an error.</p>
+  if (!data) return <p>Loading...</p>
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Message message="simple message" />
+        <Message />
         <a
           className="App-link"
           href="https://reactjs.org"
